c4a6f879616820d9a39951d00dbaace5c02891d5
Haifeng Zhao <hzha7521@uni.sydney.edu.au>
2020-09-10 17:59:51 +1000

    Change to Tkinter as wxPython is a headache.

--- main.py
+++ main.py
@@ -1,104 +1,26 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
 """
-Hello World, but with more meat.
+This is a simple interface for CBIR system.
+
+Author: Haifeng Zhao
 """
+from tkinter import *
 
-import wx
+root = Tk()
 
-class HelloFrame(wx.Frame):
-    """
-    A Frame that says Hello World
-    """
+Label(root, text="图片库路径：").grid(row=0, column=0)
+Entry(root).grid(row=0, column=1)
 
-    def __init__(self, *args, **kw):
-        # ensure the parent's __init__ is called
-        super(HelloFrame, self).__init__(*args, **kw)
+Label(root, text="提取特征存储文件名称：").grid(row=1, column=0)
+label_feature_name = Label(root, text="暂时没有特征被提取").grid(row=1, column=1)
 
-        # create a panel in the frame
-        pnl = wx.Panel(self)
+Label(root, text="待识别图片：").grid(row=2, column=0)
+Entry(root).grid(row=2, column=1)
 
-        # put some text with a larger bold font on it
-        st = wx.StaticText(pnl, label="Hello World!")
-        font = st.GetFont()
-        font.PointSize += 10
-        font = font.Bold()
-        st.SetFont(font)
+Label(root, text="最大图片数量：").grid(row=3, column=0)
+Entry(root).grid(row=3, column=1)
 
-        # and create a sizer to manage the layout of child widgets
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(st, wx.SizerFlags().Border(wx.TOP|wx.LEFT, 25))
-        pnl.SetSizer(sizer)
+Label(root, text="相似图片查找结果：").grid(row=4, column=0)
 
-        # create a menu bar
-        self.makeMenuBar()
-
-        # and a status bar
-        self.CreateStatusBar()
-        self.SetStatusText("Welcome to wxPython!")
-
-
-    def makeMenuBar(self):
-        """
-        A menu bar is composed of menus, which are composed of menu items.
-        This method builds a set of menus and binds handlers to be called
-        when the menu item is selected.
-        """
-
-        # Make a file menu with Hello and Exit items
-        fileMenu = wx.Menu()
-        # The "\t..." syntax defines an accelerator key that also triggers
-        # the same event
-        helloItem = fileMenu.Append(-1, "&Hello...\tCtrl-H",
-                "Help string shown in status bar for this menu item")
-        fileMenu.AppendSeparator()
-        # When using a stock ID we don't need to specify the menu item's
-        # label
-        exitItem = fileMenu.Append(wx.ID_EXIT)
-
-        # Now a help menu for the about item
-        helpMenu = wx.Menu()
-        aboutItem = helpMenu.Append(wx.ID_ABOUT)
-
-        # Make the menu bar and add the two menus to it. The '&' defines
-        # that the next letter is the "mnemonic" for the menu item. On the
-        # platforms that support it those letters are underlined and can be
-        # triggered from the keyboard.
-        menuBar = wx.MenuBar()
-        menuBar.Append(fileMenu, "&File")
-        menuBar.Append(helpMenu, "&Help")
-
-        # Give the menu bar to the frame
-        self.SetMenuBar(menuBar)
-
-        # Finally, associate a handler function with the EVT_MENU event for
-        # each of the menu items. That means that when that menu item is
-        # activated then the associated handler function will be called.
-        self.Bind(wx.EVT_MENU, self.OnHello, helloItem)
-        self.Bind(wx.EVT_MENU, self.OnExit,  exitItem)
-        self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
-
-
-    def OnExit(self, event):
-        """Close the frame, terminating the application."""
-        self.Close(True)
-
-
-    def OnHello(self, event):
-        """Say hello to the user."""
-        wx.MessageBox("Hello again from wxPython")
-
-
-    def OnAbout(self, event):
-        """Display an About Dialog"""
-        wx.MessageBox("This is a wxPython Hello World sample",
-                      "About Hello World 2",
-                      wx.OK|wx.ICON_INFORMATION)
-
-
-if __name__ == '__main__':
-    # When this module is run (not imported) then create the app, the
-    # frame, show it, and start the event loop.
-    app = wx.App()
-    frm = HelloFrame(None, title='建筑图片聚类系统')
-    frm.Show()
-    app.MainLoop()
+root.mainloop()
